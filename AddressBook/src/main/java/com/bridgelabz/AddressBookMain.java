0c29581747f0064d9d18eab91c61b71ec9337c93
vivek <vivek.iitd93@gmail.com>
2021-08-18 08:22:42 +0530

    Display: initial comment

---
+++ AddressBook/src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/**
+ * <h1>Address Book Program</h1>
+ *
+ * @author vivek kumar
+ * @version 1.0
+ * @since 2021-08-18
+ */
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Problem");
+    }
+}
